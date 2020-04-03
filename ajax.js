17609b33c9eef25df47443961423c4fdebc418fe
Nassiwa Veronica <veronicanassiwa@gmail.com>
2020-04-04 02:58:11 +0400

    APIs wth AJAX

--- ajax.js
+++ ajax.js
@@ -1,27 +1,20 @@
-function ajc(){
-    var obj= new XMLHttpRequest;
-    obj.onreadystatechange=function (){
-        if(this.readyState==4 && this.status==200){
-            //var doc=JSON.parse(this.responseText);
-            document.getElementById("very").innerHTML=this.responseText;
+var div=document.getElementById('info');
+function myfunction(){
 
-        }
-    };
-    obj.open("GET","https://ghibliapi.herokuapp.com/films",true);
-    obj.send();
+var request=new XMLHttpRequest;
+request.open('GET','https://ghibliapi.herokuapp.com/films', true);
+request.onload=function(){
+    if(this.status==200){
+    var resp=JSON.parse(this.responseText);
+    htmlstring(resp);
+};
 }
-// how to convert json data to javascript object
-/*
-function myfunc(){
-    var objj='{"name":"vero","age":"38","address":"abuDhabi"}';
-    var conv=JSON.parse(objj);
-    document.getElementById("very").innerHTML=conv.name + " from"+ " "+  conv.address;
-}var
-// how to convert from javascript object to json 
-
-function jsn(){
-var ojj={name:"vero",age:"38",address:"abuDhabi"};
-var jsn2=JSON.stringify(ojj);
-document.getElementById("very").innerHTML=jsn2;
+request.send();
 }
- */
+function htmlelement(data){
+    var htmlstring="";
+    for(var i=0;i<data.length;i++){
+    htmlstring += "<p>" + data[i].title + "</p>";
+    }
+    div.insertAdjacentHTML('Beforeend','htmlstring ');
+}
